a6a244e38baf130ce7e28057a34ae4e5a5055285
laura cabos <lauracabos@gmail.com>
2024-06-05 22:20:39 +0200

    CSS corrections

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+Flat.destroy_all
+puts "Let's go"
+
+4.times do
+  Flat.create!(
+    name: Faker::Hipster.word,
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.paragraph,
+    price_per_night: rand(1..10_000),
+    number_of_guests: rand(1..10)
+  )
+end
+
+puts "It's done"
